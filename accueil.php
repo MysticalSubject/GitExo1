50861ef362baa6beeb747e793fb82afaba4930fc
Matteo FLORE <matteo.flore@lmdsio.fr>
2025-09-17 14:46:22 +0200

    Version 2

--- accueil.php
+++ accueil.php
@@ -7,4 +7,25 @@ if (isset($_POST['prenom']) && isset($_POST['nom'])) {
     echo "Veuillez remplir le formulaire correctement.";
 }
 ?>
+<?php
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+if (isset($_POST['login']) && isset($_POST['password'])) {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    if ($login === 'admin' && $password === 'azerty') {
+        header("Location: http://$host$uri/profil.html");
+        exit();
+    } else {
+        header("Location: http://$host$uri/index.html");
+        exit();
+    }
+} else {
+    header("Location: http://$host$uri/index.html");
+    exit();
+}
+?>
+
 
